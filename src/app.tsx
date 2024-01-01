42ba0e94d23a1446b2f371e1dc006d003fcba727
Steven Ludvickson <ludvickson@gmail.com>
2024-01-01 13:50:48 -0800

    fixed logic w/out useEffect; app is functional

--- src/app.tsx
+++ src/app.tsx
@@ -11,17 +11,28 @@ const App: React.FC = () => {
     const [intervalID, setIntervalID] = useState(null)
     
     const startTimer = () => {
+        incrementPhase()
         setIntervalID(setInterval(() => setTime(t => t + 0.1), 100))
+    }
+
+    const stopTimer = () => {
+        incrementPhase()
+        clearInterval(intervalID)
+    }
+
+    const reset = () => {
+        incrementPhase()
+        setTime(0)
     }
 
     const Phases = ["Click to Start", "Stop", "Reset"]
 
     const PhaseFunctions = {
-        '0': startTimer(),
+        '0': startTimer,
 
-        '1': clearInterval(intervalID),
+        '1': stopTimer,
 
-        '2': setTime(0)
+        '2': reset
     };
     
     const incrementPhase = () => {
